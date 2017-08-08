9687b5eabaf8c3870a6a85ac7793f02fceb52954
Pete Corey <petecorey@gmail.com>
2017-08-08 15:11:24 -0400

    First commit

---
+++ lib/hello_bitcoin.ex
@@ -0,0 +1,18 @@
+defmodule HelloBitcoin do
+  @moduledoc """
+  Documentation for HelloBitcoin.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HelloBitcoin.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ node.exs
@@ -0,0 +1,14 @@
+['seed.bitcoin.sipa.be',
+ 'dnsseed.bluematt.me',
+ 'dnsseed.bitcoin.dashjr.org',
+ 'seed.bitcoinstats.com',
+ 'seed.bitcoin.jonasschnelli.ch',
+ 'seed.btc.petertodd.org']
+|> List.first
+|> :inet_res.lookup(:in, :a)
+|> List.first
+|> :gen_tcp.connect(8333, [:binary, active: true])
+|> case do
+     {:ok, socket} -> IO.puts("Connected!")
+     {:error, reason} -> IO.puts("Error connecting #{inspect reason}")
+   end
